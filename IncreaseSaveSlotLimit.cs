c5b5df0dfcafce34df67d1873db17c9029eea658
zeobviouslyfakeacc <zeobviouslyfakeacc@users.noreply.github.com>
2019-09-15 19:50:15 +0200

    Initial commit, version 1.0

---
+++ IncreaseSaveSlotLimit.cs
@@ -0,0 +1,5 @@
+﻿internal class IncreaseSaveSlotLimit {
+	public static void OnLoad() {
+		SaveGameSlots.MAX_SAVESLOTS = 999;
+	}
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("IncreaseSaveSlotLimit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IncreaseSaveSlotLimit")]
+[assembly: AssemblyCopyright("MIT License")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("b80b03c0-a6cd-4a70-808b-35c93e1b614d")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
