740d8c82f2f7be19365a29957dd7f29ef05f090e
ds5678 <jrpritts7@gmail.com>
2021-01-14 14:33:02 -0500

    updated license

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -14,5 +14,5 @@ using MelonLoader;
 [assembly: Guid("b80b03c0-a6cd-4a70-808b-35c93e1b614d")]
 [assembly: AssemblyVersion("1.1.0.0")]
 [assembly: AssemblyFileVersion("1.1.0.0")]
-[assembly: MelonInfo(typeof(IncreaseSaveSlotLimit.Implementation), "IncreaseSaveSlotLimit", "1.1", "zeobviouslyfake, ds5678")]
+[assembly: MelonInfo(typeof(IncreaseSaveSlotLimit.Implementation), "IncreaseSaveSlotLimit", "1.1", "zeobviouslyfakeacc, ds5678")]
 [assembly: MelonGame("Hinterland", "TheLongDark")]
